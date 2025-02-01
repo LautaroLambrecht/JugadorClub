a63407db8b051516bd7b1c6125a81b01b8a88d26
louta <lalaxx@alumnatflorida.es>
2025-02-01 20:58:49 +0100

    Terminado clase Futbol

--- futbol.php
+++ futbol.php
@@ -2,4 +2,26 @@
 
     include_once "deporte.php";
 
+    class Futbol extends Deporte {
+
+        private $tiempoJuego;
+
+        public function __construct($nombre = "Futbol", $numeroJugadores = 11, $tiempoJuego = 90){
+            parent::__construct($nombre, $numeroJugadores);
+            $this->tiempoJuego = $tiempoJuego;
+        }
+
+        public function getNombre(){
+            parent::getNombre();
+        }
+
+        public function getNumeroJugadores(){
+            parent::getNumeroJugadores();
+        }
+
+        public function getTiempoJuego(){
+            echo $this->tiempoJuego;
+        }
+    }
+
 ?>
